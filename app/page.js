db6437ff040c41b5c9927b99e2d5d8aa50ac00dd
klader-digital <hello@klader.digital>
2023-03-22 11:10:29 +0100

    Added `package-lock.json`.

--- app/page.js
+++ app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
     <ReactLenis
       root
       options={{
-        duration: 2.5,
+        duration: 1,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
         direction: 'vertical', // vertical, horizontal
         gestureDirection: 'vertical', // vertical, horizontal, both
